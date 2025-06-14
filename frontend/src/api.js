71146bf8370be03d291157048693ed5d084deea0
naresh mahiya <nareshmahiya2017@gmail.com>
2025-06-14 16:07:01 +0530

    vercel deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080/',
+  baseURL: import.meta.env.VITE_BACKEND_URL ,
   withCredentials: true,
 });
 export default api;
